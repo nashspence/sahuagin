0a42254b6b60c68034dfbd45f569dba846e61fe5
Nash Spence <nashspence@gmail.com>
2025-01-22 02:06:44 +0000

    add clap dependency

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(name = "sahuagin", version = "0.1.0", about = "Generator")]
+struct Cli {
+    /// Input file
+    input: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+    println!("Using input file: {}", args.input);
 }
