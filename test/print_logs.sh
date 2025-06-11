cfdeb4369e6bf2a4487d4c47afc89df8e0271a7e
Nash Spence <nashspence@gmail.com>
2025-06-10 19:03:35 -0700

    Clean up repo to focus on notebook

--- test/print_logs.sh
+++
@@ -1,9 +0,0 @@
-PGPASSWORD="test" psql -h localhost -p 5432 -U postgres -d sahuagin -t -A -c "
-SELECT json_agg(
-    json_build_object(
-        'id', id,
-        'log_time', log_time,
-        'procedure_name', procedure_name,
-        'log_message', log_message
-    )
-) FROM debug_log;"
